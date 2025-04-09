9918e3a658f125986ed65915019c855d06b08514
MilanaShamaeva <157360971+Milana32@users.noreply.github.com>
2025-04-09 22:37:54 +0300

    initial 3 files

---
+++ ReducedFraction.java
@@ -0,0 +1,165 @@
+package reducedfractionapp;
+
+import java.util.Objects;
+
+/** Несократимая дробь.
+ */
+public class ReducedFraction {
+
+    /* =========================== Свойства =============================== */
+
+    /* ---------------------- Числитель и знаменатель --------------------- */
+    private int numerator = 1;
+    private int denominator;
+
+
+    /* =========================== Операции ============================== */
+
+    /* ---------------------------- Порождение ---------------------------- */
+
+    /** Создание дроби с указанием ее числителя и знаменателя. **/
+    public ReducedFraction(int numerator, int denominator)
+    {
+        if (denominator < 0)
+        {
+            this.numerator = numerator * -1;
+            this.denominator = denominator * -1;
+        }
+        else
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        if (denominator == 0)
+        {
+            throw new IllegalArgumentException("Denominator cannot be zero.");
+        }
+        reduce();
+    }
+
+    /** Создание дроби на основе целого числа. **/
+    public ReducedFraction(int number)
+    {
+        this.numerator = number;
+    }
+
+    /* ---------------------------- Доступ к параметрам ---------------------------- */
+    /** Вывести значение числителя **/
+    public int getNumerator() { return this.numerator; }
+
+    /** Вывести значение знаменателя **/
+    public int getDenominator() { return this.denominator; }
+
+    /* --------------------- Арифметические операции ---------------------- */
+
+    /** Сложение двух дробей. **/
+    public ReducedFraction add(ReducedFraction other)
+    {
+        int newDenominator = this.denominator * other.denominator;
+        int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+
+        return new ReducedFraction(newNumerator, newDenominator);
+    }
+
+    /** Вычитание двух дробей. **/
+    public ReducedFraction sub(ReducedFraction other)
+    {
+        int newDenominator = this.denominator * other.denominator;
+        int newNumerator = this.numerator * other.denominator - other.numerator * this.denominator;
+
+        return new ReducedFraction(newNumerator, newDenominator);
+    }
+
+    /** Умножение двух дробей. **/
+    public ReducedFraction mult(ReducedFraction other)
+    {
+        int newNumerator = this.numerator * other.numerator;
+        int newDenominator = this.denominator * other.denominator;
+
+        return new ReducedFraction(newNumerator, newDenominator);
+    }
+
+
+    /** Деление двух дробей. **/
+    public ReducedFraction div(ReducedFraction other)
+    {
+        if (other.numerator == 0)
+        {
+            throw new IllegalArgumentException("You can't divide on zero");
+        }
+
+        int newNumerator = this.numerator * other.denominator;
+        int newDenominator = this.denominator * other.numerator;
+
+        return new ReducedFraction(newNumerator, newDenominator);
+    }
+
+    /** Сокращение дроби **/
+
+    private int gcd(int numerator, int denominator) // Нахождение НОД
+    {
+        if (denominator == 0)
+        {
+            return numerator;
+        }
+        return gcd(denominator, numerator % denominator);
+    }
+
+    private void reduce() // Сокращение дроби
+    {
+        int gcd = this.gcd(this.numerator, this.denominator);
+        this.numerator /= gcd;
+        this.denominator /= gcd;
+
+        // Знаменатель должен быть положительным
+        if (this.denominator < 0)
+        {
+            this.numerator = -this.numerator;
+            this.denominator = -this.denominator;
+        }
+    }
+    /* --------------------- Операции сравнения ---------------------- */
+
+    /** Сравнение двух дробей. **/
+    public int toCompare(ReducedFraction other)
+    {
+        // Приводим дроби к общему знаменателю и сравниваем числители
+        int left = this.numerator * other.denominator;
+        int right = other.numerator * this.denominator;
+
+        return Integer.compare(left, right);
+    }
+
+    /** Эквивалентность двух дробей **/
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ReducedFraction that = (ReducedFraction) o;
+        return numerator == that.numerator && denominator == that.denominator;
+    }
+
+
+
+    /* --------------------- Операции преобразования ---------------------- */
+
+    /** Представить как строку. **/
+    @Override
+    public String toString ()
+    {
+        if (denominator == 1) // Для целого числа
+        {
+            return Integer.toString(this.numerator);
+        }
+        else // Для дроби
+        {
+            return Integer.toString(this.numerator) + "/" + Integer.toString(this.denominator);
+        }
+    }
+
+    /** Представить как вещественное число. **/
+    public double toDouble()
+    {
+        return (double) numerator / denominator;
+    }
+}
---
+++ Shamaeva.java
@@ -0,0 +1,117 @@
+package point3d;
+
+public final class Point3D
+{
+    private final double x;
+    private final double y;
+    private final double z;
+
+
+    private Point3D(double x, double y, double z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+
+    private Point3D(){this(0, 0, 0);}
+
+
+    public double getX()
+    {
+        return x;
+    }
+
+    public double getY()
+    {
+        return y;
+    }
+
+    public double getZ()
+    {
+        return z;
+    }
+
+
+    private double r;
+    private double theta;
+    private double phi;
+
+    public double getR() {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+    public double getTheta() {
+        return Math.acos(z / getR());
+    }
+
+    public double getPhi() {
+        return Math.atan2(y, x);
+    }
+
+
+
+    public static Point3D fromPolar(double r, double theta, double phi)
+    {
+
+        double x = r * Math.sin(theta) * Math.cos(phi);
+        double y = r * Math.sin(theta) * Math.sin(phi);
+        double z = r * Math.cos(theta);
+        return new Point3D(x, y, z);
+    }
+
+    public static Point3D fromDec(double x, double y, double z)
+    {
+        return new Point3D(x, y, z);
+    }
+
+
+
+    public double distanceTo(Point3D other)
+    {
+        double dx = other.x - this.x;
+        double dy = other.y - this.y;
+        double dz = other.z - this.z;
+        return Math.sqrt(dx * dx + dy * dy + dz * dz);
+    }
+
+
+    public double distanceToOrigin()
+    {
+        return Math.sqrt(x * x + y * y + z * z);
+    }
+
+
+    public Point3D move(double dx, double dy, double dz)
+    {
+        return new Point3D(x + dx, y + dy, z + dz);
+    }
+
+
+    public Point3D scale(double factor)
+    {
+        return new Point3D(x * factor, y * factor, z * factor);
+    }
+
+
+    public boolean equals(Point3D other, double precision)
+    {
+
+        return Math.abs(this.x - other.x) < precision &&
+                Math.abs(this.y - other.y) < precision &&
+                Math.abs(this.z - other.z) < precision;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("point3d.Point3D(x=%.2f, y=%.2f, z=%.2f)", x, y, z);
+    }
+}
+
+
+
+
+
+
